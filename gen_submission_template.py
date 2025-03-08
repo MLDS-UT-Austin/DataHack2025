b8c659943bbdda8846d372f62d8900f32f513b25
Justin Sasek <justinsasek@gmail.com>
2025-03-07 23:09:01 -0600

    added script to generate submission

---
+++ gen_submission_template.py
@@ -0,0 +1,46 @@
+import os
+import re
+from glob import glob
+
+import pandas as pd
+
+ANSWERS_PATH = "example_dataset_with_submission_damage/submission.csv"
+EVENT_PATHS = "events_students/event_*.csv"
+SAVE_PATH = "../DataHack2025/submission/submission.csv"
+
+CITY="GANopolis"
+
+df = pd.read_csv(ANSWERS_PATH)
+df = df.astype("object")
+df.iloc[:, 1:] = pd.NA
+
+
+df["price"] = df["event_number"].apply(lambda x: f"<price for event {x} in {CITY}>")
+
+event_ends = []
+for event_path in glob(EVENT_PATHS):
+    event_id = re.match(r".*event_(\d+).csv", event_path).group(1)
+    event_df = pd.read_csv(event_path)
+    end = event_df["hour"].max().item()
+    event_ends.append((event_id, end))
+
+event_ends.sort(key=lambda x: int(x[0]))
+event_ends = dict(event_ends)
+
+# <wind_speed for event 1 at hour 8095>
+df["0"] = df["event_number"].apply(
+    lambda x: f"<wind_speed for event {x} in {CITY} at hour {event_ends[str(x)]+1}>"
+)
+df["1"] = df["event_number"].apply(
+    lambda x: f"<wind_speed for event {x} in {CITY} at hour {event_ends[str(x)]+2}>"
+)
+
+df.to_csv(SAVE_PATH, index=False)
+
+with open(SAVE_PATH, "r") as f:
+    content = f.read()
+
+content = re.sub(r",,+", ", ...", content)
+
+with open(SAVE_PATH, "w") as f:
+    f.write(content)
